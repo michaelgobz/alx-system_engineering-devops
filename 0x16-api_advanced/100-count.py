29e11031921490643903deba622d57545e14208d
michaelgobz <raniahgobz@outlook.com>
2022-11-04 12:44:58 +0000

    fixed the request count and now adding web stack monitoring

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ Recursive function that queries the Reddit API, parses the title of all
 hot articles, and prints a sorted count of given keywords (case-insensitive,
 delimited by spaces. Javascript should count as javascript,
